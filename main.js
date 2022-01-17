1e0ec130f53f03df87b391a9e8850e082000e85c
Mrinaaldabaria <82158515+Mrinaaldabaria@users.noreply.github.com>
2022-01-17 12:52:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function bed_room() {
+    window.location = "bed_room.html";
+}
+
+function tv_ac() {
+    window.location = "tv_ac.html";
+}
+
+function desk() {
+    window.location = "desk.html";
+}
+
+function bottles() {
+    window.location = "bottles.html";
+}
+
+function fruit_baskets() {
+    window.location = "fruit_baskets.html";
+}
